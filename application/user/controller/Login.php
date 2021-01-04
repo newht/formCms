8a2ecb9ba7988bd9e1604cbe4ad8ea3e180df57a
16665 <1666588717@qq.com>
2021-01-04 17:13:45 +0800

    添加企业入口登录模块

--- application/user/controller/Login.php
+++ application/user/controller/Login.php
@@ -5,6 +5,7 @@ namespace app\user\controller;
 use app\user\model\User;
 use think\Controller;
 use think\Request;
+use think\Db;
 
 class Login extends Controller
 {
@@ -34,5 +35,17 @@ class Login extends Controller
         return $this -> fetch("login/unit_login");
     }
 
-
+    public function unitLoignVerifty()
+    {
+        $unitcode = input('unitcode');
+        $password = input('password');
+        $data = Db::name('unit') -> where('unitcode',$unitcode) -> find();
+        if( empty($data) ){
+            return json(['code'=>0,'msg'=>'登录失败,账号不存在','errorname'=>'unitcode','url'=>null]);
+        }
+        if( $data['password'] !== $password ){
+            return json(['code'=>0,'msg'=>'登录失败,密码错误','errorname'=>'password','url'=>null]);
+        }
+        return json(['code'=>1,'msg'=>'登录成功','errorname'=>null,'url'=>'/']);
+    }
 }
