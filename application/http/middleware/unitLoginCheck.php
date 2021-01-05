01f8ca7c54a836e3f661b953003ee40803058acc
16665 <1666588717@qq.com>
2021-01-05 16:20:44 +0800

    验证单位是否登录

---
+++ application/http/middleware/unitLoginCheck.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\http\middleware;
+
+use think\Request;
+
+class unitLoginCheck
+{
+    public function handle(Request $request, \Closure $next)
+    {
+        if (empty(session('unit'))){
+            return redirect('/user/login');
+        }
+        return $next($request);
+    }
+}
+
